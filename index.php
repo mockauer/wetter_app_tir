5b117dce5aa76e5ddb43afec62ed9dc04447d44d
slehmann <s.lehmann@esemos.de>
2018-12-11 16:39:40 +0100

    EDT: Wetterbegriffe fertig machen

--- index.php
+++ index.php
@@ -7,25 +7,30 @@
 		case 0:
 			$wetter = "sonnig";
 			echo "sonnig";
+			break;
 		case 1:
 			$wetter = "leicht bedeckt";
 			echo "leicht bedeckt";
+			break;
 		case 2:
 			$wetter = "stark bedeckt";
 			echo "stark bedeckt";
+			break;
 		case 3:
 			$wetter = "regen";
 			echo "regen";
+			break;
 		case 4:
 			$wetter = "sturm";
-			echo "sturm"
+			echo "sturm";
+			break;
 		case 5: 
 			$wetter = "gewitter";
-			echo "gewitter"
-			
+			echo "gewitter";
+			break;
 		}
 		
-		echo $wetter;
+		
 	
 	
 ?>

35a15fee08157cb1e4baf9d1135644de58bcb179
slehmann <s.lehmann@esemos.de>
2018-12-12 10:50:21 +0100

    EDT: Anpassen der Anzeige

--- index.php
+++ index.php
@@ -1,44 +1,12 @@
 <?php
 	
-	$zufallszahl = rand(0,4);
-	$wetter = "";
-	$time = time();
-
-	$monate = array(1,2,3,4,5,6,7,8,9,10,11,12);
-	$akt_monat = date("m", $time);
+	include("db_connect.php");
 	
-	$temperatur = array(rand(86,182),rand(104,199),rand(86,176),rand(66,147),rand(32,108),rand(24,94),rand(15,86),rand(15,85),rand(21,105),rand(34,124),rand(58,152),rand(68,142));
 	
-	foreach ($monate as $monat){		
-		if ($monat == $akt_monat) {
-			echo $temperatur[$monat-1]/10 ."<br/>";
-		}
-	}
-	
-	switch ($zufallszahl){
-		case 0:
-			$wetter = "sonnig";
-			echo "<img src='img/sonnig.png' />";
-			break;
-		case 1:
-			$wetter = "leicht bedeckt";
-			echo "<img src='img/leichtbewolkt.png' />";
-			break;
-		case 2:
-			$wetter = "stark bedeckt";
-			echo "<img src='img/starkbewolkt.png' />";
-			break;
-		case 3:
-			$wetter = "regen";
-			echo "<img src='img/regen.png' />";
-			break;
-		case 4: 
-			$wetter = "gewitter";
-			echo "<img src='img/gewitter.png' />";
-			break;
-		}
+		$query ="SELECT * FROM `wetter` ORDER BY `wetter`.`datum` DESC ";
+		$all=$mysqli->query($query);
 		
-		
-	
-	
+		 while ($dsatz=  $all->fetch_assoc()){
+			  $dsatz['datum'] . " - " . $dsatz['wetter'] . " - ".$dsatz['temperatur'];
+		 }
 ?>
